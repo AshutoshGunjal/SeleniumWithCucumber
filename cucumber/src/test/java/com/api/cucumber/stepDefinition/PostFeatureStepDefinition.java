06495dafd8ce0dc58df0b97c6e852468a8ee07ff
LAPTOP-12ON9ABD\ASHUTOSH <ashutosh.r.gunjal@gmail.com>
2020-09-30 18:49:54 -0400

    PostFeature step definition file added

---
+++ cucumber/src/test/java/com/api/cucumber/stepDefinition/PostFeatureStepDefinition.java
@@ -0,0 +1,50 @@
+package com.api.cucumber.stepDefinition;
+
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class PostFeatureStepDefinition {
+	 @Given("^User should be logged in $")
+	    public void user_should_be_logged_in() throws Throwable {
+	        throw new PendingException();
+	    }
+
+	    @When("^I type the post in the text wall$")
+	    public void i_type_the_post_in_the_text_wall() throws Throwable {
+	        throw new PendingException();
+	    }
+
+	    @When("^User supply the YouTube link in the textbox$")
+	    public void user_supply_the_youtube_link_in_the_textbox() throws Throwable {
+	        throw new PendingException();
+	    }
+
+	    @Then("^The message should get posted$")
+	    public void the_message_should_get_posted() throws Throwable {
+	        throw new PendingException();
+	    }
+
+	    @Then("^Video should posted on user wall$")
+	    public void video_should_posted_on_user_wall() throws Throwable {
+	        throw new PendingException();
+	    }
+
+	    @And("^should be present at its own wall$")
+	    public void should_be_present_at_its_own_wall() throws Throwable {
+	        throw new PendingException();
+	    }
+
+	    @And("^Click on post button$")
+	    public void click_on_post_button() throws Throwable {
+	        throw new PendingException();
+	    }
+
+	    @And("^Video should have proper Thumbnail$")
+	    public void video_should_have_proper_thumbnail() throws Throwable {
+	        throw new PendingException();
+	    }
+
+}
