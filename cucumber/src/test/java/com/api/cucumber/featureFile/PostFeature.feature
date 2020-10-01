ca32fbfff7a7cc57b6c65d683b366e61126508ef
LAPTOP-12ON9ABD\ASHUTOSH <ashutosh.r.gunjal@gmail.com>
2020-10-01 10:10:31 -0400

    pasing paramter in cucumber step

--- cucumber/src/test/java/com/api/cucumber/featureFile/PostFeature.feature
+++ cucumber/src/test/java/com/api/cucumber/featureFile/PostFeature.feature
@@ -4,13 +4,14 @@ Feature: Post feature of facebook
 Background: Given User should be logged in 
 	And should be present at its own wall 
 
+
 Scenario: Post a text on user wall
-	When I type the post in the text wall
+	When I type the post as "This is a sample post" in the text wall
 	And Click on post button
 	Then The message should get posted
 	
 Scenario: Post a video on user wall 
-	When User supply the YouTube link in the textbox
+	When User supply the YouTube link as "http:google.com" in the textbox
 	And Click on post button
 	Then Video should posted on user wall
 	And Video should have proper Thumbnail
--- cucumber/src/test/java/com/api/cucumber/stepDefinition/PostFeatureStepDefinition.java
+++ cucumber/src/test/java/com/api/cucumber/stepDefinition/PostFeatureStepDefinition.java
@@ -6,44 +6,44 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class PostFeatureStepDefinition {
-	 @Given("^User should be logged in $")
-	    public void user_should_be_logged_in() throws Throwable {
-	        System.out.println("Given User should be logged in");
-	    }
+	@Given("^User should be logged in $")
+	public void user_should_be_logged_in() throws Throwable {
+		System.out.println("Given User should be logged in");
+	}
 
-	    @When("^I type the post in the text wall$")
-	    public void i_type_the_post_in_the_text_wall() throws Throwable {
-	        System.out.println("When I type the post in the text walls");
-	    }
+	@When("^I type the post as \"([^\"]*)\" in the text wall$")
+	    public void i_type_the_post_as_something_in_the_text_wall(String strArg1) throws Throwable {
+	        System.out.println("Value : " + strArg1);
+	}
 
-	    @When("^User supply the YouTube link in the textbox$")
-	    public void user_supply_the_youtube_link_in_the_textbox() throws Throwable {
-	        System.out.println("When User supply the YouTube link in the textbox");
-	    }
+	@When("^User supply the YouTube link as \"([^\"]*)\" in the textbox$")
+	public void user_supply_the_youtube_link_as_something_in_the_textbox(String strArg1) throws Throwable {
+		System.out.println("Value : " + strArg1);
+	}
 
-	    @Then("^The message should get posted$")
-	    public void the_message_should_get_posted() throws Throwable {
-	        System.out.println("Then The message should get posted");
-	    }
+	@Then("^The message should get posted$")
+	public void the_message_should_get_posted() throws Throwable {
+		System.out.println("Then The message should get posted");
+	}
 
-	    @Then("^Video should posted on user wall$")
-	    public void video_should_posted_on_user_wall() throws Throwable {
-	        System.out.println("Then Video should posted on user wall");
-	    }
+	@Then("^Video should posted on user wall$")
+	public void video_should_posted_on_user_wall() throws Throwable {
+		System.out.println("Then Video should posted on user wall");
+	}
 
-	    @And("^should be present at its own wall$")
-	    public void should_be_present_at_its_own_wall() throws Throwable {
-	        System.out.println("And should be present at its own walls");
-	    }
+	@And("^should be present at its own wall$")
+	public void should_be_present_at_its_own_wall() throws Throwable {
+		System.out.println("And should be present at its own walls");
+	}
 
-	    @And("^Click on post button$")
-	    public void click_on_post_button() throws Throwable {
-	        System.out.println("And Click on post button");
-	    }
+	@And("^Click on post button$")
+	public void click_on_post_button() throws Throwable {
+		System.out.println("And Click on post button");
+	}
 
-	    @And("^Video should have proper Thumbnail$")
-	    public void video_should_have_proper_thumbnail() throws Throwable {
-	        System.out.println("And Video should have proper Thumbnail");
-	    }
+	@And("^Video should have proper Thumbnail$")
+	public void video_should_have_proper_thumbnail() throws Throwable {
+		System.out.println("And Video should have proper Thumbnail");
+	}
 
 }
